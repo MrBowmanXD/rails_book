634c829037131f9c46e5962b4c233a02d5c5ebac
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-12-06 10:49:31 +0000

    Added devise gem and posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # root "static_pages#home"
+  resources :posts
+  devise_for :users
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
